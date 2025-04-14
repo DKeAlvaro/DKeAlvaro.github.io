7e337a3460aa67de218f65646daf44c8f1b04d57
Alvaro <alvaro.mrgr@gmail.com>
2025-04-14 11:26:36 +0200

    Update configuration for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // vite.config.ts
 export default defineConfig({
     plugins: [react()],
+    base: '/',
     server: {
       watch: {
         usePolling: true,
