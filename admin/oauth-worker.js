4ea64c23ecbd5b23ac94ceb013d05cf2b8012a6c
Alvaro <91745263+DKeAlvaro@users.noreply.github.com>
2026-04-28 22:24:17 +0200

    Oauth

--- admin/oauth-worker.js
+++ admin/oauth-worker.js
@@ -3,65 +3,21 @@ export default {
     const url = new URL(request.url);
     const path = url.pathname;
 
-    const corsHeaders = {
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type',
-    };
-
-    if (request.method === 'OPTIONS') {
-      return new Response(null, { headers: corsHeaders });
-    }
-
     if (path === '/auth') {
-      const params = new URLSearchParams({
-        client_id: env.GITHUB_CLIENT_ID,
-        redirect_uri: url.origin + '/callback',
-        scope: 'repo',
-        response_type: 'code',
-      });
-      const githubUrl = 'https://github.com/login/oauth/authorize?' + params.toString();
-      return new Response('', {
-        status: 302,
-        headers: { Location: githubUrl, ...corsHeaders },
-      });
+      const p = new URLSearchParams({ client_id: env.GITHUB_CLIENT_ID, scope: 'repo', response_type: 'code' });
+      return Response.redirect('https://github.com/login/oauth/authorize?' + p.toString(), 302);
     }
-
     if (path === '/callback') {
       const code = url.searchParams.get('code');
-      if (!code) {
-        return new Response('<html><body>No code</body></html>', {
-          status: 200,
-          headers: { 'Content-Type': 'text/html', ...corsHeaders },
-        });
-      }
-
       const r = await fetch('https://github.com/login/oauth/access_token', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json', Accept: 'application/json' },
-        body: JSON.stringify({
-          client_id: env.GITHUB_CLIENT_ID,
-          client_secret: env.GITHUB_CLIENT_SECRET,
-          code,
-        }),
+        body: JSON.stringify({ client_id: env.GITHUB_CLIENT_ID, client_secret: env.GITHUB_CLIENT_SECRET, code }),
       });
-
       const j = await r.json();
-
-      if (!j.access_token) {
-        return new Response('<html><body><h1>Error</h1><pre>' + JSON.stringify(j) + '</pre></body></html>', {
-          status: 200,
-          headers: { 'Content-Type': 'text/html', ...corsHeaders },
-        });
-      }
-
-      const token = JSON.stringify(j.access_token).slice(1, -1);
-      return new Response('<!DOCTYPE html><html><body><script>opener.postMessage("authorization:github:success:"+JSON.stringify({token:"' + token + '",provider:"github"}),"*");close();\x3c/script></body></html>', {
-        status: 200,
-        headers: { 'Content-Type': 'text/html; charset=utf-8', ...corsHeaders },
-      });
+      const html = '<html><script>opener.postMessage("authorization:github:success:"+JSON.stringify({token:"' + j.access_token + '",provider:"github"}),"*");close()<\/script></html>';
+      return new Response(html, { headers: { 'Content-Type': 'text/html' } });
     }
-
-    return new Response('Not found', { status: 404, headers: corsHeaders });
+    return new Response('', { status: 404 });
   },
-};
+}
